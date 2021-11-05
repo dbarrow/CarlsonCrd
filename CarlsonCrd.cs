f8db28cb06c1a61d012e032d537c4ee91acd5ee3
darry <darry@DARRYLDESKTOP>
2021-11-05 13:25:44 -0400

    Add project files.

---
+++ CarlsonCrd.cs
@@ -0,0 +1,53 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+
+namespace Carlson
+{
+    public class CarlsonCrd
+    {
+        internal Document acDoc = Application.DocumentManager.MdiActiveDocument;
+
+        public void CrdOpen(string filename = "test7.crd", int bOverwrite = 0, int crdType = 0)
+        {
+            string args = "(CrdOpen \"" + filename + "\" " + bOverwrite + " " + crdType +") ";
+            acDoc.SendStringToExecute(args, true, false, true);
+        }
+
+        public void CrdClose()
+        {
+            string args = "(CrdClose) ";
+            acDoc.SendStringToExecute(args, true, false, true);
+        }
+
+        public void CrdAddPoint(double northing, double easting, double elevation, string point_id, string description)
+        {
+            string args = "(setq point (CrdPointInit " + northing + " " + easting + " " + elevation + " " + "\"" + point_id + "\" \"" + description + "\")) ";
+            acDoc.SendStringToExecute(args, true, false, true);
+            args = "(CrdAddPoint point) ";
+            acDoc.SendStringToExecute(args, true, false, true);
+        }
+
+        public void CrdDraw()
+        {
+            CrdDrawOptions cdo = new CrdDrawOptions();
+            cdo.PointLayer.Value = "testLayer";
+            cdo.SymbolLayer.Value = "sybolLayer";
+            string options = null;
+
+            foreach (OptionPair op in cdo.OptionPairs)
+            {
+                if(op.Value != "")
+                {
+                    options += op.PairToString();
+                }
+            }
+            string args = "(CrdDraw '(\"Points\" \"ALL\" ";
+            if(options != null)
+            {
+                args += options;
+            }
+
+            args += ")) ";
+            acDoc.SendStringToExecute(args, true, false, true);
+        }
+    }
+}
---
+++ CrdDrawOptions.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+
+namespace Carlson
+{
+    public class CrdDrawOptions
+    {
+        public List<OptionPair> OptionPairs = new List<OptionPair>();
+        public OptionPair SymbolName = new OptionPair("SymbolName", "");
+        public OptionPair SymbolXScale = new OptionPair("SymbolXScale", "");
+        public OptionPair SymbolYScale = new OptionPair("SymbolYScale", "");
+        public OptionPair SymbolRotation = new OptionPair("SymbolRotation", "");
+        public OptionPair SymbolLayer = new OptionPair("SymbolLayer", "");
+        public OptionPair BlockName = new OptionPair("BlockName", "");
+        public OptionPair BlockXScale = new OptionPair("BlockXScale", "");
+        public OptionPair BlockYScale = new OptionPair("BlockYScale", "");
+        public OptionPair BlockRotation = new OptionPair("BlockRotation", "");
+        public OptionPair BlockNameLayer = new OptionPair("BlockNameLayer", "");
+        public OptionPair BlockElevationLayer = new OptionPair("BlockElevationLayer", "");
+        public OptionPair BlockDesriptionLayer = new OptionPair("BlockDescriptionLayer", "");
+        public OptionPair PointLayer = new OptionPair("PointLayer", "");
+        
+        public CrdDrawOptions()
+        {
+            OptionPairs.Add(SymbolName);
+            OptionPairs.Add(SymbolXScale);
+            OptionPairs.Add(SymbolYScale);
+            OptionPairs.Add(SymbolRotation);
+            OptionPairs.Add(SymbolLayer);
+            OptionPairs.Add(BlockName);
+            OptionPairs.Add(BlockXScale);
+            OptionPairs.Add(BlockYScale);
+            OptionPairs.Add(BlockRotation);
+            OptionPairs.Add(BlockNameLayer);
+            OptionPairs.Add(BlockElevationLayer);
+            OptionPairs.Add(BlockDesriptionLayer);
+            OptionPairs.Add(PointLayer);
+        }
+    }
+
+    public class OptionPair
+    {
+        public string Key;
+        public string Value { get; set; }
+        public OptionPair() { }
+        public OptionPair(string key, string val)
+        {
+            Key = key;
+            Value = val;
+        }
+
+        public string PairToString()
+        {
+            return "\"" + Key + "\" \"" + Value + "\"";
+        }
+    }
+}
